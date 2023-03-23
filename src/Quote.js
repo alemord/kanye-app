a76e2af668b058ae03a8e183d495ddde941c707b
alemord <alessandro.mordini@gmail.com>
2023-03-23 13:05:17 -0400

    complete

---
+++ src/Quote.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+
+function Quote() {
+  const [quote, setQuote] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const fetchQuote = () => {
+    setLoading(true);
+    fetch('https://api.kanye.rest')
+      .then(response => response.json())
+      .then(data => {
+        setQuote(data.quote);
+        setLoading(false);
+      })
+      .catch(error => console.error(error));
+  };
+
+  return (
+    <div>
+      <h2>Kanye Once Really Said:</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="quote-container">
+        <p>"{quote}"</p>
+        <img src="./signature.png" className="flash" />
+        <button onClick={fetchQuote}>Give Me More</button>
+      </div>
+    )}
+  </div>
+);
+}
+
+export default Quote;
